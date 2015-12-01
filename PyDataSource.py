f2c160183d0453f6af2b43f50b4708ea30aaf2dd
koglin <koglin@SLAC.STANFORD.EDU>
2015-12-01 06:52:03 +0000

    split out DataSourceInfo and update evrConfig to work with controls recording devices that also have Evr configStore keys

--- PyDataSource.py
+++ PyDataSource.py
@@ -6,38 +6,7 @@ import time
 import traceback
 import psana
 import numpy as np
-#from Detector.PyDetector import PyDetector
-
-def live_source(monshmserver='psana', **kwargs):
-    """Returns psana source string for live data from shared memory on the current node.
-       The standard convention is to have the shared memry servers either named 'psana'
-       or the instrument name in all caps.  This will return the source string for 
-       the valid one based on searching the local file system.  The process will be
-       found at:
-
-          '/dev/shm/PdsMonitorSharedMemory_'+monshmserver
-    """
-    from glob import glob
-    import os
-
-    shm_srvs = glob('/dev/shm/PdsMonitorSharedMemory_'+monshmserver)
-    if shm_srvs == []:
-        hostsplit = os.uname()[1].split('-')
-        instrument = hostsplit[1]
-        monshmserver = instrument.upper()
-        shm_srvs = glob('/dev/shm/PdsMonitorSharedMemory_'+monshmserver)
-    
-    if shm_srvs != []:
-        try:
-            MPI_RANK = 0
-            source_str = 'shmem={:}.0:stop=no'.format(monshmserver)
-        except:
-            print 'Exception in finding shared memory server: ',shm_srvs
-            source_str = None
-    else:
-        source_str = None
-
-    return source_str
+from DataSourceInfo import *
 
 def evt_dict(self):
     """Used in TabEvent and TabKeys Classes.
@@ -195,132 +164,6 @@ def get_config(configStore, attr, cls='Config'):
             return obj
 
 
-class DataSourceInfo(object):
-    """-------------------------------------------------------------------------------
-       data_source class built from keyword arguments that can be accessed as attributes
-
-            data_source = 'exp=CXI/cxic0115:run=10'
-
-        The following are equivalent:
-
-            data_source = DataSourceInfo(exp='cxic0115', run=10).data_source
-            data_source = str(DataSourceInfo(exp='cxic0115', run=10))
-
-        You can also load a data source with keyword options:
-
-            smd:  small data support -- has become standard for experiments after Oct 2015
-            h5:   loads hdf5 data instead of xtc
-            ffb:  appends ':one-stream' to data source
-
-        The shared memory data_source can be loaded with the monshmemsrver keyword:
-
-            data_source = str(DataSource(monshmemsrver='psana'))
-
-        But shared memory should alse be automatically detected if no arguments are
-        supplied and you are on a shared memery server.
-            
-            data_source = str(DataSource())
-
-    """
-    _exp_defaults = {'instrument':    None, 
-                     'exp':           None, 
-                     'h5':            None,
-                     'run':           0,
-                     'smd':           None, 
-                     'station':       0,
-                     'idx':           None,
-                     'ffb':           None,
-                     'monshmserver':  None,
-                     'indexed':       False,
-                     'dir':           None,
-                     'cfg':           None}
-
-    def __init__(self, data_source=None, **kwargs):
-        self.data_source = self._set_data_source(data_source=data_source, **kwargs)
-
-    def _set_exp_defaults(self, **kwargs):
-        """Sets experiment defaults based on kwargs and defaults.
-        """
-        for key, val in self._exp_defaults.items():
-            setattr(self, key, kwargs.get(key, val))
-
-        if self.exp is not None:
-            self.instrument = self.exp[0:3]
-
-#        inst_id = '{:}:{:}'.format(self.instrument.upper(), self.station)
-
-    def _set_data_source(self, data_source=None, **kwargs): 
-        self._set_exp_defaults(**kwargs)
-
-        if self.monshmserver:
-            self.indexed = False
-            if not data_source:
-                data_source = live_source(**kwargs)
-
-        if data_source:
-            opts = data_source.split(':')
-            for opt in opts:
-                items = opt.split('=')
-                key = items[0]
-                if key not in self._exp_defaults:
-                    self._exp_defaults.update({key: None})
-                    setattr(self, key, None)
-
-                if len(items) == 2:
-                    value = items[1]
-                    setattr(self, key, value)
-                else:
-                    setattr(self, key, True)
-                    if key in ['idx']:
-                        self.indexed = True
-
-        else:
-
-            if self.exp and self.run:
-                self.instrument = self.exp[0:3]
-     
-                data_source = "exp={exp}:run={run}".format(exp=self.exp,run=self.run)
-                
-                if self.ffb:
-                    data_source += ":one-stream"
-                elif self.h5:
-                    data_source += ":h5"
-                elif self.smd:
-                    data_source += ":smd"
-                    self.indexed = False 
-                elif self.idx:
-                    data_source += ":idx"
-                    self.indexed = True
-
-            else:
-                print 'No data source specified, so assume this is shared memory.'
-                data_source = live_source(**kwargs)
-                self.monshmserver = data_source
-                self.indexed = False
-
-
-        if not self.instrument and self.exp is not None:
-            self.instrument = self.exp[0:3]
-
-        if self.exp and self.monshmserver:
-            calibDir = '/reg/d/psdm/cxi/{:}/calib'.format(self.exp)
-            print 'setting calibDir', self.exp, calibDir
-            psana.setOption('psana.calib-dir', calibDir)
-
-
-        return data_source
-
-    def show_info(self):
-        print self.__repr__()
-        for attr in self._exp_defaults:
-            print '{:20} {:}'.format(attr, getattr(self, attr))
-
-    def __str__(self):
-        return self.data_source
-
-    def __repr__(self):
-        return '< {:}: {:} >'.format(self.__class__.__name__, self.data_source)
-
 class DataSource(object):
     """Python version of psana.DataSource with support for event and config
        data as well as PyDetector functions to access calibrated data.
@@ -366,6 +209,7 @@ class DataSource(object):
         csPartition = get_config(configStore, 'Partition')
         csEpics = get_config(configStore, 'Epics')
         csEvr = get_config(configStore, 'EvrData', cls='IOConfig')
+        self._evrConfig = csEvr
         self._bldMask = csPartition.bldMask()
         self._partition = {str(s.src()): {'src': s.src(), 'group': s.group(), 'alias': None} \
                          for s in csPartition.sources()}
@@ -401,7 +245,7 @@ class DataSource(object):
                 self._sources[srcstr] = item
 
         self.configStore = TabKeys(configStore)
-        self.evrConfig = EvrDictify(self.configStore)
+        self.evrConfig = EvrDictify(self)
 
         for srcstr, item in self._sources.items():
             alias = item.get('alias')
@@ -527,9 +371,6 @@ class Events(object):
     """Event iterator
     """
 
-    _nevents_cash = 10
-    _events = []
-
     def __init__(self, ds, **kwargs):
         self._kwargs = kwargs
         self._ds = ds
@@ -538,16 +379,12 @@ class Events(object):
         return self
 
     def next(self):
-        if len(self._events) > self._nevents_cash:
-            self._events.pop(0)
-      
         self._ds._ievent += 1
         self._ds._evtData = None
         evt = self._ds._ds.events().next()
         srcstrs = [str(key.src()) for key in evt.keys()]
         self._ds._current_aliases = [alias for alias, srcstr in self._ds._aliases.items() \
                                      if srcstr in srcstrs]
-        self._events.append(evt)
         self._ds._current_evt = evt 
 
         return EvtDetectors(self._ds)
@@ -678,7 +515,7 @@ class AliasConfig(object):
             print '{:18s} {:}'.format(alias, item['src'])
 
     def __str__(self):
-        return '{:}: {:}'.format(self.__class__.__name__, str(self))
+        return '{:}'.format(self.__class__.__name__)
     
     def __repr__(self):
         self.show_info()
@@ -1361,7 +1198,6 @@ class ReDictify(object):
     def __repr__(self):
         repr_str = '< {:}.{:} >'.format(self._obj.__class__.__module__, \
                 self._obj.__class__.__name__) 
-#        self.show_info()
         return repr_str
 
     def __getattr__(self, attr):
@@ -1412,14 +1248,13 @@ class EvrDictify(object):
         evr = EvrDictify(ds.env().configStore())
     """
 
-    def __init__(self, configStore):
-        if configStore.__class__.__name__ is not 'TabKeys':
-            configStore = TabKeys(configStore)
+    def __init__(self, ds):
         
+        configStore = ds.configStore
         self._evr_dict = {}
         self._src_dict = {}
         self._output_maps = {}
-        self._alias_dict = {str(a.src): a.aliasName for a in configStore.Alias.srcAlias}
+        alias_dict = {val: key for key, val in ds._aliases.items()}
         self._evr_keys = [attr for attr,item in configStore._evt_dict.items() \
                           if 'EvrDataConfig' in item['type']]
 
@@ -1442,7 +1277,7 @@ class EvrDictify(object):
                 map_key = 'module{:}_conn{:02}'.format(output_map.module, output_map.conn_id)
                 for i in range(ch.ninfo):
                     src = SrcDictify(ch.infos[i])
-                    src.add_property(alias=self._alias_dict.get(src.src))
+                    src.add_property(alias=alias_dict.get(src.src))
                     src.add_property(evr=self._output_maps[map_key].evr)
     #                src.add_property(output=output_map)
     #                src.add_property(map_key=map_key)
@@ -1453,7 +1288,7 @@ class EvrDictify(object):
             pass
 
         for key, item in self._evr_dict.items():
-            alias = self._alias_dict.get(key)
+            alias = alias_dict.get(key)
             if alias:
                 setattr(self, alias, item)
 
